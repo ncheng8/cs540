2f630806fbe0d0ae75be1268b29904a563e70e01
Nicholas Cheng <nickbaicheng@gmail.com>
2018-04-05 01:31:08 -0500

    Implement flag 100, start implementing segment class

--- chatbot/src/Chatbot.java
+++ chatbot/src/Chatbot.java
@@ -2,136 +2,179 @@
 import java.util.*;
 import java.io.*;
 
-public class Chatbot{
-    private static String filename = "./WARC201709_wid.txt";
-    private static ArrayList<Integer> readCorpus(){
-        ArrayList<Integer> corpus = new ArrayList<Integer>();
-        try{
-            File f = new File(filename);
-            Scanner sc = new Scanner(f);
-            while(sc.hasNext()){
-                if(sc.hasNextInt()){
-                    int i = sc.nextInt();
-                    corpus.add(i);
-                }
-                else{
-                    sc.next();
-                }
-            }
-        }
-        catch(FileNotFoundException ex){
-            System.out.println("File Not Found.");
-        }
-        return corpus;
-    }
-    static public void main(String[] args){
-        ArrayList<Integer> corpus = readCorpus();
+public class Chatbot {
+	private static String filename = "./WARC201709_wid.txt";
+
+	private static ArrayList<Integer> readCorpus() {
+		ArrayList<Integer> corpus = new ArrayList<Integer>();
+		try {
+			File f = new File(filename);
+			Scanner sc = new Scanner(f);
+			while (sc.hasNext()) {
+				if (sc.hasNextInt()) {
+					int i = sc.nextInt();
+					corpus.add(i);
+				} else {
+					sc.next();
+				}
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("File Not Found.");
+		}
+		return corpus;
+	}
+
+	static public void main(String[] args) {
+		ArrayList<Integer> corpus = readCorpus();
 		int flag = Integer.valueOf(args[0]);
-        
-        if(flag == 100){
+
+		if (flag == 100) {
 			int w = Integer.valueOf(args[1]);
-            int count = 0;
-            //TODO count occurence of w
-            //TODO remember to round to 7 digits
-            for (Integer word : corpus) {
-            	if (word == w) {
-            		count++;
-            	}
-            }
-            double prob = count/(double)corpus.size();
-            prob = (double)Math.round(prob * 10000000d) / 10000000d;
-            System.out.println(count);
-            System.out.println(String.format("%.7f", prob));
-        }
-        else if(flag == 200){
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
-            //TODO generate
+			int count = 0;
 
-        }
-        else if(flag == 300){
-            int h = Integer.valueOf(args[1]);
-            int w = Integer.valueOf(args[2]);
-            int count = 0;
-            ArrayList<Integer> words_after_h = new ArrayList<Integer>();
-            //TODO
+			for (Integer word : corpus) {
+				if (word == w) {
+					count++;
+				}
+			}
+			double prob = count / (double) corpus.size();
+			prob = (double) Math.round(prob * 10000000d) / 10000000d;
+			System.out.println(count);
+			System.out.println(String.format("%.7f", prob));
+		} else if (flag == 200) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
+			double r = n1 * 1.0 / n2;
+			int[] test1 = {1,2,3};
+			Segment potato = new Segment(3,test1, 5.0, 10.0);
+			// TODO generate
+			int[] test2 = potato.wordIndex();
+			for (int p : test2) {
+				System.out.println(p);
+			}
 
-            //output 
-            System.out.println(count);
-            System.out.println(words_after_h.size());
-            System.out.println(String.format("%.7f",count/(double)words_after_h.size()));
-        }
-        else if(flag == 400){
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
-            int h = Integer.valueOf(args[3]);
-            //TODO
-            
-        }
-        else if(flag == 500){
-            int h1 = Integer.valueOf(args[1]);
-            int h2 = Integer.valueOf(args[2]);
-            int w = Integer.valueOf(args[3]);
-            int count = 0;
-            ArrayList<Integer> words_after_h1h2 = new ArrayList<Integer>();
-            //TODO
+		} else if (flag == 300) {
+			int h = Integer.valueOf(args[1]);
+			int w = Integer.valueOf(args[2]);
+			int count = 0;
+			ArrayList<Integer> words_after_h = new ArrayList<Integer>();
+			// TODO
 
-            //output 
-            System.out.println(count);
-            System.out.println(words_after_h1h2.size());
-            if(words_after_h1h2.size() == 0)
-                System.out.println("undefined");
-            else
-                System.out.println(String.format("%.7f",count/(double)words_after_h1h2.size()));
-        }
-        else if(flag == 600){
-            int n1 = Integer.valueOf(args[1]);
-            int n2 = Integer.valueOf(args[2]);
-            int h1 = Integer.valueOf(args[3]);
-            int h2 = Integer.valueOf(args[4]);
-            //TODO
-        }
-        else if(flag == 700){
-            int seed = Integer.valueOf(args[1]);
-            int t = Integer.valueOf(args[2]);
-            int h1=0,h2=0;
+			// output
+			System.out.println(count);
+			System.out.println(words_after_h.size());
+			System.out.println(String.format("%.7f", count / (double) words_after_h.size()));
+		} else if (flag == 400) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
+			int h = Integer.valueOf(args[3]);
+			// TODO
 
-            Random rng = new Random();
-            if (seed != -1) rng.setSeed(seed);
+		} else if (flag == 500) {
+			int h1 = Integer.valueOf(args[1]);
+			int h2 = Integer.valueOf(args[2]);
+			int w = Integer.valueOf(args[3]);
+			int count = 0;
+			ArrayList<Integer> words_after_h1h2 = new ArrayList<Integer>();
+			// TODO
 
-            if(t == 0){
-                // TODO Generate first word using r
-                double r = rng.nextDouble();
-                System.out.println(h1);
-                if(h1 == 9 || h1 == 10 || h1 == 12){
-                    return;
-                }
+			// output
+			System.out.println(count);
+			System.out.println(words_after_h1h2.size());
+			if (words_after_h1h2.size() == 0)
+				System.out.println("undefined");
+			else
+				System.out.println(String.format("%.7f", count / (double) words_after_h1h2.size()));
+		} else if (flag == 600) {
+			int n1 = Integer.valueOf(args[1]);
+			int n2 = Integer.valueOf(args[2]);
+			int h1 = Integer.valueOf(args[3]);
+			int h2 = Integer.valueOf(args[4]);
+			// TODO
+		} else if (flag == 700) {
+			int seed = Integer.valueOf(args[1]);
+			int t = Integer.valueOf(args[2]);
+			int h1 = 0, h2 = 0;
 
-                // TODO Generate second word using r
-                r = rng.nextDouble();
-                System.out.println(h2);
-            }
-            else if(t == 1){
-                h1 = Integer.valueOf(args[3]);
-                // TODO Generate second word using r
-                double r = rng.nextDouble();
-                System.out.println(h2);
-            }
-            else if(t == 2){
-                h1 = Integer.valueOf(args[3]);
-                h2 = Integer.valueOf(args[4]);
-            }
+			Random rng = new Random();
+			if (seed != -1)
+				rng.setSeed(seed);
 
-            while(h2 != 9 && h2 != 10 && h2 != 12){
-                double r = rng.nextDouble();
-                int w  = 0;
-                // TODO Generate new word using h1,h2
-                System.out.println(w);
-                h1 = h2;
-                h2 = w;
-            }
-        }
+			if (t == 0) {
+				// TODO Generate first word using r
+				double r = rng.nextDouble();
+				System.out.println(h1);
+				if (h1 == 9 || h1 == 10 || h1 == 12) {
+					return;
+				}
 
-        return;
-    }
+				// TODO Generate second word using r
+				r = rng.nextDouble();
+				System.out.println(h2);
+			} else if (t == 1) {
+				h1 = Integer.valueOf(args[3]);
+				// TODO Generate second word using r
+				double r = rng.nextDouble();
+				System.out.println(h2);
+			} else if (t == 2) {
+				h1 = Integer.valueOf(args[3]);
+				h2 = Integer.valueOf(args[4]);
+			}
+
+			while (h2 != 9 && h2 != 10 && h2 != 12) {
+				double r = rng.nextDouble();
+				int w = 0;
+				// TODO Generate new word using h1,h2
+				System.out.println(w);
+				h1 = h2;
+				h2 = w;
+			}
+		}
+
+		return;
+	}
+}
+
+class Segment {
+	private int type;
+	private int[] index;
+	private double lhs;
+	private double rhs;
+
+	public Segment(int type, int[] index, double lhs, double rhs) {
+		if (type == 1) {
+			this.index = new int[1];
+			this.index[0] = index[0];
+
+		} else if (type == 2) {
+			this.index = new int[2];
+			this.index[0] = index[0];
+			this.index[1] = index[1];
+		} else {
+			this.index = new int[3];
+			this.index[0] = index[0];
+			this.index[1] = index[1];
+			this.index[2] = index[2];
+		}
+		this.type = type;
+		this.lhs = lhs;
+		this.rhs = rhs;
+
+	}
+
+	public double getLHS() {
+		return lhs;
+	}
+
+	public double getRHS() {
+		return rhs;
+	}
+	
+	public int[] wordIndex() {
+		return index;
+	}
+	
+	public int[] binarySearch() {
+		return null;
+	}
 }
