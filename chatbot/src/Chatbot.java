47ced23438682853771fc8ef345b8463f200020a
Nicholas Cheng <nickbaicheng@gmail.com>
2018-04-07 23:25:56 -0500

    Working part b and segment system

--- chatbot/src/Chatbot.java
+++ chatbot/src/Chatbot.java
@@ -41,18 +41,44 @@ public class Chatbot {
 			prob = (double) Math.round(prob * 10000000d) / 10000000d;
 			System.out.println(count);
 			System.out.println(String.format("%.7f", prob));
+
 		} else if (flag == 200) {
 			int n1 = Integer.valueOf(args[1]);
 			int n2 = Integer.valueOf(args[2]);
 			double r = n1 * 1.0 / n2;
-			int[] test1 = {1,2,3};
-			Segment potato = new Segment(3,test1, 5.0, 10.0);
-			// TODO generate
-			int[] test2 = potato.wordIndex();
-			for (int p : test2) {
-				System.out.println(p);
+			double currPos = 0;
+			// list of all segments; 4700, one for each word
+			ArrayList<Segment> allIntervals = new ArrayList<Segment>();
+			// instantiate all the segments, put a word in each one
+			// this way, the list is ordered based on word
+			for (int i = 0; i <= 4700; i++) {
+				int[] t = { i };
+				allIntervals.add(new Segment(1, t));
 			}
-
+			// go through corpus and update the word counts as we go
+			for (Integer t : corpus)
+				allIntervals.get(t).addCount();
+			// calculate the l and r values for each segment based on their counts 
+			// and the size of the corpus
+			for (Segment s : allIntervals) {
+				s.setLHS(currPos);
+				currPos += s.getCount() / (double) corpus.size();
+				s.setRHS(currPos);
+			}
+			// search through the segment list and find the "randomly chosen" word
+			for (Segment s : allIntervals) {
+				if (r <= s.getRHS() && r > s.getLHS()) {
+					System.out.println(s.wordIndex()[0]);
+					System.out.println(String.format("%.7f", s.getLHS()));
+					System.out.println(String.format("%.7f", s.getRHS()));
+				}
+			}
+			//try binary search
+			int index = binarySearch(allIntervals, r);
+			Segment s = allIntervals.get(index);
+			System.out.println(s.wordIndex()[0]);
+			System.out.println(String.format("%.7f", s.getLHS()));
+			System.out.println(String.format("%.7f", s.getRHS()));
 		} else if (flag == 300) {
 			int h = Integer.valueOf(args[1]);
 			int w = Integer.valueOf(args[2]);
@@ -133,11 +159,34 @@ public class Chatbot {
 
 		return;
 	}
+
+	public static int binarySearch(ArrayList<Segment> list, double x) {
+		// out of bounds check
+		if (x < 0 || x >= list.size()) {
+			return -1;
+		}
+		int l = 0;
+		int r = list.size() - 1;
+		int mid;
+		while (l <= r) {
+			mid = (l + r) / 2;
+			if (x <= list.get(mid).getLHS()) { // search the left half
+				r = mid - 1;
+			} else if (x > list.get(mid).getRHS()) { // search the right half
+				l = mid + 1;
+			} else { // return if the middle value is the one we're looking for
+				return mid;
+			}
+		}
+		return -1;
+	}
 }
 
 class Segment {
 	private int type;
 	private int[] index;
+	private int word;
+	private int count = 0;
 	private double lhs;
 	private double rhs;
 
@@ -162,6 +211,28 @@ class Segment {
 
 	}
 
+	public Segment(int word) {
+		if (type == 1) {
+			this.index = new int[1];
+			this.index[0] = index[0];
+
+		} else if (type == 2) {
+			this.index = new int[2];
+			this.index[0] = index[0];
+			this.index[1] = index[1];
+		} else {
+			this.index = new int[3];
+			this.index[0] = index[0];
+			this.index[1] = index[1];
+			this.index[2] = index[2];
+		}
+		this.type = type;
+		/*
+		 * this.lhs = lhs; this.rhs = rhs;
+		 */
+
+	}
+
 	public double getLHS() {
 		return lhs;
 	}
@@ -169,12 +240,24 @@ class Segment {
 	public double getRHS() {
 		return rhs;
 	}
-	
+
 	public int[] wordIndex() {
 		return index;
 	}
-	
-	public int[] binarySearch() {
-		return null;
+
+	public void addCount() {
+		this.count++;
+	}
+
+	public void setLHS(double x) {
+		this.lhs = x;
+	}
+
+	public void setRHS(double x) {
+		this.rhs = x;
+	}
+
+	public int getCount() {
+		return count;
 	}
 }
